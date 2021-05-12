e2220313150f3b21d0663dfa571574ba731935b3
Andreas Achilleos <andreasachilleos1994@gmail.com>
2021-05-12 19:05:50 +0100

    Completing components challenge

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45pm"
+          comment="Amazing post, keep up the work!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00am"
+          comment="Woah my mind is blown!!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 6:49pm"
+          comment="Holy moly what a beautiful thing!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      {/*This is how to make individual children different*/}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
